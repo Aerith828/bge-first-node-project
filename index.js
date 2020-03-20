d3ad02e1fad3cc09c6d2ca7b67654f76383ad92d
Aerith828 <58862394+Aerith828@users.noreply.github.com>
2020-03-20 11:11:15 +0100

    creation about.html + public

--- index.js
+++ index.js
@@ -7,14 +7,4 @@ app.listen(3000, () => {
     console.log("SERVER STARTED...");
 });
 
-app.get('/' , (req, res) => {
-    console.log("access to / path");
-    const html = fs
-        .readFileSync("./src/index.html")
-        .toString('utf8');
-    res.send(html);
-});
-
-app.get("/about" , (req,res) => {
-    res.send("about");
-})
+app.use(express.static('./public'));
